d2faa4d5a3a11732fba1301a5a3abc78e680241a
AuspiciousCrane <99872706+AuspiciousCrane@users.noreply.github.com>
2022-02-24 17:51:48 +0700

    Did showdisk somewhat

--- Q3/disk.py
+++ Q3/disk.py
@@ -1,3 +1,5 @@
+import turtle
+
 class Disk(object):
     def __init__(self, name="", xpos = 0, ypos = 0, height = 20, width = 40):
         self.dname = name
@@ -7,10 +9,30 @@ class Disk(object):
         self.dwidth = width
 
     def showdisk(self):
-        pass
+        screen = turtle.getscreen()
+        turtle.setheading(0)
+        turtle.penup()
+        turtle.goto(self.dxpos, self.dypos)
+        turtle.pendown()
+
+        turtle.begin_fill()
+        for i in range(2):
+            turtle.forward(self.dwidth / 2)
+            turtle.lt(90)
+            turtle.forward(self.dheight)
+            turtle.lt(90)
+            turtle.forward(self.dwidth / 2)
+        turtle.end_fill()
 
     def newpos(self, xpos, ypos):
         pass
 
     def cleardisk(self):
+        pass
+
+
+if __name__ == "__main__":
+    d = Disk(xpos = 100, ypos = 0)
+    d.showdisk()
+    while(1):
         pass
