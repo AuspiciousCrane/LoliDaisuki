0642864dc6c440c0694401ee47c08291e172bf75
Janthanat Thanintantrakun <beamnut@hotmail.com>
2022-02-24 16:49:47 +0700

    feat: question1

--- Q2/main.py
+++ Q2/main.py
@@ -1,1 +1,23 @@
-Never Gonna Give You Up
+import sys
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+
+class SimplePaintProgram(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Paint Program")
+        self.setGeometry(0,0,360,220)
+
+        self.show()
+
+def main():
+    app = QApplication(sys.argv)
+    
+    w = SimplePaintProgram()
+    w.show()
+
+    return app.exec()
+
+if __name__ == "__main__":
+    sys.exit(main())
