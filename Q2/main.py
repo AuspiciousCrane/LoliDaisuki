285f1177071d956e2a525bd723d0dbb67855d12b
Janthanat Thanintantrakun <beamnut@hotmail.com>
2022-02-24 17:53:19 +0700

    feat: question2 gui

--- Q2/main.py
+++ Q2/main.py
@@ -7,13 +7,23 @@ class SimplePaintProgram(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Paint Program")
-        self.setGeometry(0,0,360,220)
+        self.setGeometry(0,0,360,180)
+        layout = QVBoxLayout()
+        layout1 = QHBoxLayout()
+        layout1.setGeometry(QRect(100,100,100,100))
         self.image = QImage(self.size(), QImage.Format_RGB32)
         self.image.fill(Qt.white)
 
+        self.label = QLabel()
+        self.label.setText("Drag the mouse to draw")
+        self.label.setAlignment(Qt.AlignCenter)
+
         self.clear_button = QPushButton("Clear", self)
-        self.clear_button.setGeometry(10, 170, 340, 40)
+        self.clear_button.setGeometry(10, 130, 340, 40)
         
+        layout1.addWidget(self.label)
+        layout.addLayout(layout1)
+        self.setLayout(layout)
 
         self.show()
 
