78949eb9ab89d723afb10a15e27621870b4c8d4c
Chatchai222 <65013350+Chatchai222@users.noreply.github.com>
2022-02-24 16:58:21 +0700

    Added Simple_drawing_window1

--- Q1/main.py
+++ Q1/main.py
@@ -43,10 +43,39 @@ class Simple_drawing_window2(Simple_drawing_window):
         self.setWindowTitle("Simple Drawing 2")
         self.rabbit = QPixmap("Q1\images\play_icon.png")
 
+class Simple_drawing_window1(Simple_drawing_window):
+    def __init__(self):
+        QWidget.__init__(self,None)
+        self.setWindowTitle("Simple drawing 1")
+        self.gun = QPixmap("Q1\images\gun.png")
+        
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+ 
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([
+            QPoint( 70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 150),
+        ])
+ 
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+        
+        p.drawPolygon(
+            [QPoint( 50, 200), QPoint(150, 200), QPoint(100, 400),]
+            )
+ 
+        p.drawPixmap(QRect(200, 100, 320, 320), self.gun)
+        p.end()
+
+
 def main():
     app = QApplication(sys.argv)
 
-    w = Simple_drawing_window2()
+    w = Simple_drawing_window1()
     w.show()
 
     return app.exec_()
